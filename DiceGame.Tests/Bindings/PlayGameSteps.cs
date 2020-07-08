87468848f1e21f642dc7edf1c587597837cafabd
Benjamin Hempel <benjamin.hempel@saxsys.de>
2020-07-08 14:59:27 +0200

    Add first UI testing steps

--- DiceGame.Tests/Bindings/PlayGameSteps.cs
+++ DiceGame.Tests/Bindings/PlayGameSteps.cs
@@ -3,32 +3,46 @@ using System.Collections.Generic;
 using System.Text;
 using TechTalk.SpecFlow;
 using FlaUI.UIA3;
+using FlaUI.Core;
+using FlaUI.Core.AutomationElements;
+using System.Linq;
+using System.Threading;
 
 namespace DiceGame.Tests.Bindings
 {
     [Binding]
     class PlayGameSteps
     {
+        private readonly UIA3Automation Automation = new UIA3Automation();
+        private Application App;
+        private Window MainWindow;
+
         [Given(@"the dice game application is started")]
         public void GivenTheDiceGameApplicationIsStarted()
         {
-            ScenarioContext.Current.Pending();
+            App = Application.Launch("../../../../DiceGame/bin/Debug/netcoreapp3.1/DiceGame.exe");
+            App.WaitWhileMainHandleIsMissing(TimeSpan.FromSeconds(1));
+            MainWindow = App.GetAllTopLevelWindows(Automation).First();
         }
 
         [When(@"I select the ""(.*)"" digit and toss")]
-        public void WhenISelectTheDigitAndToss(string p0)
+        public void WhenISelectTheDigitAndToss(string digitName)
         {
-            ScenarioContext.Current.Pending();
+            var radio = MainWindow.FindFirstDescendant(x => x.ByText(digitName)).AsRadioButton();
+            radio.IsChecked = true;
+
+            var tossButton = MainWindow.FindFirstDescendant(x => x.ByAutomationId("TossButton")).AsButton();
+            tossButton.Invoke();
         }
 
         [Then(@"the result for player ""(.*)"" should not contain any zeroes")]
-        public void ThenTheResultForPlayerShouldNotContainAnyZeroes(string p0)
+        public void ThenTheResultForPlayerShouldNotContainAnyZeroes(string playerName)
         {
             ScenarioContext.Current.Pending();
         }
 
         [Then(@"the result for player ""(.*)"" should be (.*) digits long")]
-        public void ThenTheResultForPlayerShouldBeDigitsLong(string p0, int p1)
+        public void ThenTheResultForPlayerShouldBeDigitsLong(string playerName, int digitCount)
         {
             ScenarioContext.Current.Pending();
         }
